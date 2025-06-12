587fa4058a98e6d11c68f092551092f17fa1db23
Mofifoluwa Akinwande <153274843+boredfifo@users.noreply.github.com>
2025-06-12 08:41:20 +0200

    Update controlFSM.vhd

--- Traffic_Cross_Management/VHDL/controlFSM.vhd
+++ Traffic_Cross_Management/VHDL/controlFSM.vhd
@@ -3,9 +3,11 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity controlFSM is
+
 port(clk, en, rst, ne1, ne2, se1, se2, sw1, sw2, nw1, nw2: IN STD_LOGIC;
-nwTOsw, swTOse, neTOse, nwTOne: OUT STD_LOGIC;
+nwTOsw, swTOse, neTOse, nwTOne, reqOutput: OUT STD_LOGIC;
 nOutput, sOutput, eOutput, wOutput: OUT STD_LOGIC_VECTOR (1 downto 0) );
+
 end controlFSM;
 
 architecture behavioral of controlFSM is
@@ -21,17 +23,25 @@ component signalStates
 end component;
 
 component pedestrianStates
+	generic(GREEN_CYCLES: integer:= 5);
 	port(clk, en, rst, control: IN STD_LOGIC;
 		output: OUT STD_LOGIC);
 end component;
 
-type state is (NSGREEN, EWGREEN, NSRED, EWRED);
+component D_FLIPFLOP is
+port(d, clk, rst, en: IN STD_LOGIC;
+	q: OUT STD_LOGIC);
+end component;
+
+type state is (NS_ACTIVE, EW_ACTIVE);
 signal current_state, next_state: state;
-signal  nControl, sControl, eControl, wControl, nwSWcontrol, swSEcontrol, neSEcontrol, nwNEcontrol: STD_LOGIC;
+signal  nControl, sControl, eControl, wControl, nwSWcontrol, swSEcontrol, neSEcontrol, nwNEcontrol, request, corr: STD_LOGIC;
+signal corr_cnt : integer range 0 to 3 := 0;
+signal nsOutput: STD_LOGIC_VECTOR (1 downto 0);
 begin
 northLight: signalStates
 	generic map (RED_CYCLES => 5, YELLOW_CYCLES => 2, GREEN_CYCLES  => 5) 
-	port map (clk => clk, en=>en, rst=>rst, control=> nControl, output => nOutput);
+	port map (clk => clk, en=>en, rst=>rst, control=> nControl, output => nsOutput);
 southLight: signalStates
 	generic map (RED_CYCLES => 5, YELLOW_CYCLES => 2, GREEN_CYCLES  => 5) 
 	port map (clk => clk, en=>en, rst=>rst, control=> sControl, output => sOutput);
@@ -41,54 +51,43 @@ eastLight: signalStates
 westLight: signalStates
 	generic map (RED_CYCLES => 5, YELLOW_CYCLES => 2, GREEN_CYCLES  => 5) 
 	port map (clk => clk, en=>en, rst=>rst, control=> wControl, output => wOutput);
-nwANDswPedLight: pedestrianStates port map (clk => clk, en => en, rst => rst, control => nwSWcontrol , output => nwTOsw);
-swANDsePedLight: pedestrianStates port map (clk => clk, en => en, rst => rst, control => swSEcontrol , output => swTOse);
-neANDsePedLight: pedestrianStates port map (clk => clk, en => en, rst => rst, control => neSEcontrol , output => neTOse);
-nwANDnePedLight: pedestrianStates port map (clk => clk, en => en, rst => rst, control => nwNEcontrol , output => nwTOne);
+nwANDswPedLight: pedestrianStates generic map (GREEN_CYCLES =>5)
+			port map (clk => clk, en => en, rst => rst, control => nwSWcontrol , output => nwTOsw);
+swANDsePedLight: pedestrianStates generic map (GREEN_CYCLES =>5) port map (clk => clk, en => en, rst => rst, control => swSEcontrol , output => swTOse);
+neANDsePedLight: pedestrianStates generic map (GREEN_CYCLES =>5) port map (clk => clk, en => en, rst => rst, control => neSEcontrol , output => neTOse);
+nwANDnePedLight: pedestrianStates generic map (GREEN_CYCLES =>5) port map (clk => clk, en => en, rst => rst, control => nwNEcontrol , output => nwTOne);
+nsRequestFF: D_FLIPFLOP port map (d=>corr, clk=>clk, en=>en, rst=>rst, q=>request);
+nOutput <= nsOutput;
+reqOutput <= request;
 
-next_state_logic: process(current_state)
+test: process(clk,rst)
 begin
-  --nControl    <= '0';
-  --sControl    <= '0';
-  --eControl    <= '0';
-  --    <= '0';
-  --nwSWcontrol <= '0';
-  --swSEcontrol <= '0';
-  --neSEcontrol <= '0';
-  --nwNEcontrol <= '0';
+if rst = '1' then 
+	corr <= '0';
+else
+		if(ne1 = '1' or se1 = '1') athen 
+			corr <= '1';
+		end if;
+end if;
+end process;
 
+next_state_logic: process(current_state, request)
+begin
 case (current_state) is
-	when NSGREEN => 
-		nControl <= '1'; sControl <= '1'; eControl<='0'; wControl <= '0';
-		if (ne1 = '1') or (se1 = '1') then 
-			neSEcontrol <= '1';
-		end if;
-		if (nw1 = '1') or (sw1 = '1') then
-			nwSWcontrol <= '1';
-		end if;
-		next_state <= NSRED;
-	when NSRED =>
-		nControl <= '0'; sControl <= '0'; eControl<='1'; wControl <= '1';neSEcontrol <= '0';nwSWcontrol <= '0' ;
-		next_state <= EWGREEN;
-	when EWGREEN =>
-		nControl <= '0'; sControl <= '0'; eControl<='1'; wControl <= '1';
-		if (nw2 = '1') or (ne2 = '1') then
-			nwNEcontrol <= '1';
-		end if;
-		if (sw2 = '1') or (se2 = '1') then
-			swSEcontrol <= '1';
-		end if;
-		next_state <= EWRED;
-	when EWRED =>
-		nControl <= '1'; sControl <= '1'; eControl<='0'; wControl <= '0';nwNEcontrol <= '0';swSEcontrol <= '0';
-		next_state <= NSGREEN;
+	when NS_ACTIVE => nControl <= '1'; sControl <= '1'; eControl <= '0'; wControl <= '0';
+			if (request = '1') and ((nsOutput = "01") or (nsOutput = "10"))then
+				neSEcontrol <= '1';
+			end if;
+			next_state <= EW_ACTIVE;
+	when EW_ACTIVE => nControl <= '0'; sControl <= '0'; eControl <= '1'; wControl <= '1'; neSEcontrol <= '0';
+			next_state <= NS_ACTIVE;
 end case;
 end process;
 
 next_state_memory: process(clk, rst)
 begin
 if rst = '1' then
-	current_state <= NSGREEN;
+	current_state <= NS_ACTIVE;
 elsif clk'event and clk = '1' then
 	if en = '1' then
 		current_state <= next_state;
