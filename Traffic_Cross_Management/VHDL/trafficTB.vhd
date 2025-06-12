1e03a2a473a7075acf67055227d79fc37f398a1a
Mofifoluwa Akinwande <153274843+boredfifo@users.noreply.github.com>
2025-06-12 08:43:05 +0200

    Update trafficTB.vhd

--- Traffic_Cross_Management/VHDL/trafficTB.vhd
+++ Traffic_Cross_Management/VHDL/trafficTB.vhd
@@ -6,6 +6,11 @@ end trafficTB;
 
 architecture behavioral of trafficTB is
 component signalStates
+generic(
+    RED_CYCLES    : integer := 5;
+    YELLOW_CYCLES : integer :=  2;
+    GREEN_CYCLES  : integer := 7
+  );
 port(clk, en, rst, control: IN STD_LOGIC;
 	output: OUT STD_LOGIC_VECTOR (1 downto 0));
 end component;
@@ -14,12 +19,14 @@ signal clk_tb, en_tb, rst_tb, control_tb: STD_LOGIC;
 signal output_tb: STD_LOGIC_VECTOR (1 downto 0);
 
 begin
-DUT50: signalStates port map (clk => clk_tb, en => en_tb, rst => rst_tb, output => output_tb, control => control_tb);
+DUT50: signalStates 
+generic map (RED_CYCLES => 5, YELLOW_CYCLES => 2, GREEN_CYCLES  => 7)
+port map (clk => clk_tb, en => en_tb, rst => rst_tb, output => output_tb, control => control_tb);
 
 --clock generation
 clock_process: process
 begin
-while now <200 ns loop
+while now <500 ns loop
 	clk_tb <= '0';
 	wait for 10 ns;
 	clk_tb <= '1';
@@ -40,9 +47,9 @@ begin
 	en_tb <= '1';
 	rst_tb <= '0';
 	control_tb <= '0';
-	wait for 50 ns;
+	wait for 100 ns;
 	control_tb <= '1';
-	wait for 50 ns;
+	wait for 200 ns;
 	wait;
 end process;
 end behavioral;
