410207bfe109c4906df4a60745c034ad3404a747
Mofifoluwa Akinwande <153274843+boredfifo@users.noreply.github.com>
2025-06-12 08:42:35 +0200

    Update trafficLightStates.vhd

--- Traffic_Cross_Management/VHDL/trafficLightStates.vhd
+++ Traffic_Cross_Management/VHDL/trafficLightStates.vhd
@@ -3,6 +3,11 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity signalStates is
+generic(
+    RED_CYCLES    : integer := 5;
+    YELLOW_CYCLES : integer :=  2;
+    GREEN_CYCLES  : integer := 7
+  );
 port(clk, en, rst, control: IN STD_LOGIC;
 	output: OUT STD_LOGIC_VECTOR (1 downto 0));
 end signalStates;
@@ -10,37 +15,62 @@ end signalStates;
 architecture behavioral of signalStates is
 type state is (RED, GREEN, YELLOW);
 signal current_state, next_state: state;
+signal timer, next_timer : integer range 0 to integer'high := 0;
 
 begin
 
-next_state_logic: process(en, current_state)
+next_state_logic: process(current_state, timer, control)
 begin
-case current_state is
-	when RED => next_state <= YELLOW;
-	when YELLOW => 
-		if control = '1' then
-		next_state <= GREEN;
-		elsif control = '0' then
-		next_state <= RED;
-		end if;
-	when GREEN => next_state <= YELLOW;
+case (current_state) is 
+	when RED =>
+        if timer < (RED_CYCLES - 1) then
+          next_state <= RED;
+          next_timer <= timer + 1;
+        else
+          next_state <= YELLOW;
+          next_timer <= 0;
+        end if;
+
+      when YELLOW =>
+        if timer < (YELLOW_CYCLES - 1) then
+          next_state <= YELLOW;
+          next_timer <= timer + 1;
+        else
+          if control = '1' then
+            next_state <= GREEN;
+          else
+            next_state <= RED;
+          end if;
+          next_timer <= 0;
+        end if;
+
+      when GREEN =>
+        if timer < (GREEN_CYCLES - 1) then
+          next_state <= GREEN;
+          next_timer <= timer + 1;
+       else
+          next_state <= YELLOW;
+          next_timer <= 0;
+        end if;
 end case;
 end process;
 
 next_state_memory: process(clk, rst)
 begin
-        if CLK'event and CLK = '1' then
-            if RST = '1' then
-                current_state <= RED;
-            elsif EN = '1' then
-                current_state <= next_state;
-            end if;
-        end if;
-    end process;
+        if rst = '1' then
+		current_state <= RED;
+		timer <= 0;
+	elsif clk'event and clk = '1' then
+		if en = '1' then 
+			current_state <= next_state;
+			timer <= next_timer;
+		end if;
+	end if;
+end process;
 
-output_logic: process(en, current_state)
+output_logic: process(current_state)
 begin
-	case current_state is
+	case (current_state) is
 	when GREEN => output <= "10";
 	when YELLOW => output <= "01";
 	when RED => output <= "00";
@@ -49,5 +79,4 @@ end process;
 
 end behavioral;
 	
-
 
