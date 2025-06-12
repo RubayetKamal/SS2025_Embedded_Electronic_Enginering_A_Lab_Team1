a609b0e57522a6fef9e16cc3a0e81f0a2bac2d98
Rubayet Kamal <114564297+RubayetKamal@users.noreply.github.com>
2025-06-12 09:54:22 +0200

    Update main.c

--- Traffic_Cross_Management/FreeRTOS_Simulation/main.c
+++ Traffic_Cross_Management/FreeRTOS_Simulation/main.c
@@ -7,23 +7,14 @@
 #include "queue.h"
 #include <stdlib.h>
 #include "FreeRTOSConfig.h"
+#include "hooks.h"
 
 extern QueueHandle_t northQueue;
 extern QueueHandle_t southQueue;
 extern QueueHandle_t eastQueue;
 extern QueueHandle_t westQueue;
 
-// Called when FreeRTOS malloc fails
-void vApplicationMallocFailedHook(void) {
-    fprintf(stderr, "Malloc failed!\n");
-    while (1);  // Loop forever for debug
-}
 
-// Called when a task overflows its stack
-void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
-    fprintf(stderr, "Stack overflow in task: %s\n", pcTaskName);
-    while (1);  // Loop forever for debug
-}
 
 int main() {
     initTrafficLights();
@@ -35,8 +26,8 @@ int main() {
     westQueue  = xQueueCreate(10, sizeof(Car));
 
     xTaskCreate(intersectionManager, "Intersection", 1024, NULL, 2, NULL);
-    xTaskCreate(carGeneratorTask, "CarGen", 1024, NULL, 1, NULL);
 
+    xTaskCreate(carGeneratorTask, "CarGen", 1024, NULL, 1, NULL);
     xTaskCreate(pedestrianGeneratorTask, "PedGen", 512, NULL, 1, NULL);
 
     static Direction d1 = NORTH, d2 = SOUTH, d3 = EAST, d4 = WEST;
